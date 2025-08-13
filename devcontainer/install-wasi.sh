28ec3c416e31b48df7d0834744f56dd815e77478
Brett Cannon <brett@python.org>
2025-08-13 10:43:23 -0700

    Symlink `/opt/wasi-sdk` instead of directly unpacking to it This is to use the `latest` label in the CPython repo thanks to `Toosl/wasm/wasi` looking for unpacked WASI SDK installs in Python 3.15 and 3.14.
    Also bump the version of wasmtime while we are here.

--- devcontainer/install-wasi.sh
+++ devcontainer/install-wasi.sh
@@ -3,9 +3,6 @@
 # Install the WASI SDK.
 # This should be portable to any Linux, but is only tested in the devcontainer.
 
-
-mkdir ${WASI_SDK_PATH}
-
 case "${TARGETARCH}" in
     amd64) WASI_ARCH="x86_64" ;;
     arm64) WASI_ARCH="arm64" ;;
@@ -14,7 +11,10 @@ esac && \
 
 URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_SDK_VERSION}/wasi-sdk-${WASI_SDK_VERSION}.0-${WASI_ARCH}-linux.tar.gz
 
-curl --location $URL | tar --strip-components 1 --directory ${WASI_SDK_PATH} --extract --gunzip
+curl --location $URL | tar --directory /opt --extract --gunzip
+
+# For Python 3.13 as Tools/wasm/wasi.py expects /opt/wasi-sdk.
+ln -s /opt/wasi-sdk-${WASI_SDK_VERSION}.0*/ /opt/wasi-sdk
 
 
 mkdir --parents ${WASMTIME_HOME}
